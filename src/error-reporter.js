78e3837779e45198e8b9bd2a5663cb7be3915b56
Gilbert <gilbertbgarza@gmail.com>
2015-12-22 00:51:30 -0600

    Slightly better type error messages

--- src/error-reporter.js
+++ src/error-reporter.js
@@ -1,8 +1,40 @@
+var fs = require('fs')
 var util = require('util')
 var pretty = require('./pretty')
 
 exports.report = function (ast, env, typeErr) {
-  console.log(typeErr.message)
+
+  var leftType  = typeErr.leftType
+  var rightType = typeErr.rightType
+  var leftNode  = typeErr.leftNode
+  var rightNode = typeErr.rightNode
+
+  var lpt = pretty.type(leftType)
+  var rpt = pretty.type(rightType)
+
+  var message = `
+  Your code has a type mismatch between \`${lpt}\` and \`${rpt}\`!
+
+    You have ${ pretty.node(leftType.source, { article: true }) }
+      with type \`${ lpt }\`
+      at ${ lineNo(leftNode.loc) }
+      located here:
+
+    ${ fileSource(leftNode.loc) }
+    ${ colPointer(leftNode.loc) }
+
+    And you have ${ pretty.node(rightType.source, { article: true }) }
+      with type \`${ rpt }\`
+      at ${ lineNo(rightNode.loc) }
+      located here:
+
+    ${ fileSource(rightNode.loc) }
+    ${ colPointer(rightNode.loc) }
+
+  These two usages do not match. Please adjust your code accordingly!
+`
+
+  console.log(message)
 }
 
 exports.TypeError = function TypeError (env, leftType, rightType) {
@@ -13,10 +45,37 @@ exports.TypeError = function TypeError (env, leftType, rightType) {
   this.leftType = leftType
   this.rightType = rightType
 
-  this.message = `
-cannot unify \`${pretty.type(leftType)}\` and \`${pretty.type(rightType)}\`
-    ${leftType.tag}: ${pretty.node(leftType.source)} at line ${leftType.source.loc.start.line} col ${leftType.source.loc.start.column+1}
-    ${rightType.tag}: ${pretty.node(rightType.source)} at line ${rightType.source.loc.start.line} col ${rightType.source.loc.start.column+1}
-`
+  this.leftNode = leftType.source
+  this.rightNode = rightType.source
 }
 util.inherits(exports.TypeError, Error)
+
+
+var fileCache = {}
+function fileSource (loc) {
+  var file = fileCache[loc.source] || fs.readFileSync(loc.source, 'utf8')
+  return file.split('\n')[ loc.start.line-1 ]
+}
+
+function colPointer (loc) {
+  var pointer = ''
+  for (var i=0; i < loc.start.column; i++) {
+    pointer += ' '
+  }
+  for (var i=loc.start.column; i < loc.end.column; i++) {
+    pointer += '^'
+  }
+  return pointer
+}
+
+function lineNo (loc) {
+  return `${loc.source}:${loc.start.line}:${loc.start.column+1}`
+}
+
+function hr (size) {
+  var line = ''
+  for (var i=0; i < size; i++) {
+    line += '-'
+  }
+  return line
+}
--- src/pretty.js
+++ src/pretty.js
@@ -3,7 +3,7 @@ exports.type = prettyType
 exports.node = prettyNode
 exports.env  = prettyEnv
 
-function prettyType (type) {
+function prettyType (type, options) {
   switch (type.tag) {
     case 'TermString':
       return 'String'
@@ -20,18 +20,20 @@ function prettyType (type) {
   }
 }
 
-function prettyNode (node) {
+function prettyNode (node, options) {
+  var article = options && options.article
+
   switch (node.type) {
     case 'Literal':
-      if (typeof node.value === 'string') return `'${node.value}'`
-      if (typeof node.value === 'number') return `${node.value}`
+      if (typeof node.value === 'string') return `${article ? 'a string ' : ''}'${node.value}'`
+      if (typeof node.value === 'number') return `${article ? 'a number ' : ''}${node.value}`
       return `literal ${node.value}`
 
     case 'Identifier':
-      return `variable \`${node.name}\``
+      return `${article ? 'a ' : ''}variable \`${node.name}\``
 
     case 'BinaryExpression':
-      return `Expression with binary operator \`${node.operator}\``
+      return `An expression with binary operator \`${node.operator}\``
 
     default:
       console.log("Unknown node:", node)
@@ -43,7 +45,7 @@ function prettyEnv (env) {
   var output = ""
   for (var varName in env.typings) {
     var typing = env.typings[varName]
-    output += `${ varName }: ${ prettyType(typing.type) }\n`
+    output += `    ${ varName }: ${ prettyType(typing.type) }\n`
   }
   return output
 }
